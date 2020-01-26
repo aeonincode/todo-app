7666e1db455986ad7b376cf111659681884992a3
khashayar <khashayar.sasanni@hotmail.com>
2020-01-26 18:33:26 +0100

    finishing lesson 35 and application, also password replaced with * do security reason

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let db
 
 app.use(express.static('public'))
 
-let connectionString = 'mongodb+srv://todoAppUser:****@cluster0-4r5dr.mongodb.net/TodoApp?retryWrites=true&w=majority'
+let connectionString = 'mongodb+srv://todoAppUser:******@cluster0-4r5dr.mongodb.net/TodoApp?retryWrites=true&w=majority'
 mongodb.connect(connectionString, {useNewUrlParser: true}, function(err, client) {
   db = client.db()
   app.listen(3000)
@@ -39,18 +39,13 @@ app.get('/', function(req, res) {
         </div>
         
         <ul id="item-list" class="list-group pb-5">
-          ${items.map(function(item) {
-            return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">${item.text}</span>
-            <div>
-              <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>`
-          }).join('')}
         </ul>
         
       </div>
+
+    <script>
+    let items = ${JSON.stringify(items)}
+    </script>
 
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>  
     <script src="/browser.js"></script>
