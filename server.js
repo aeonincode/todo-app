47b38d06eeb7204fd31e7d1ba476bf2ac030a905
khashayar <khashayar.sasanni@hotmail.com>
2020-01-26 14:39:57 +0100

    update until end of lesson 33, password remove in order to security reason and replaced with***

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 let express = require('express')
+let mongodb = require('mongodb')
 
 let app = express()
+let db
 
+app.use(express.static('public'))
+
+let connectionString = 'mongodb+srv://todoAppUser:******@cluster0-4r5dr.mongodb.net/TodoApp?retryWrites=true&w=majority'
+mongodb.connect(connectionString, {useNewUrlParser: true}, function(err, client) {
+  db = client.db()
+  app.listen(3000)
+})
+
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
-    res.send(`<!DOCTYPE html>
+    db.collection('items').find().toArray(function(err, items) {
+      res.send(`<!DOCTYPE html>
     <html>
     <head>
       <meta charset="UTF-8">
@@ -27,38 +39,40 @@ app.get('/', function(req, res) {
         </div>
         
         <ul class="list-group pb-5">
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #1</span>
+          ${items.map(function(item) {
+            return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">${item.text}</span>
             <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
+              <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+              <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
             </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #2</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #3</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
+          </li>`
+          }).join('')}
         </ul>
         
       </div>
-      
+
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>  
+    <script src="/browser.js"></script>
     </body>
     </html>`)
+    })
 })
 
 app.post('/create-item', function(req, res) {
-    console.log(req.body.item)
-    res.send("Thanks for submitting the form")
+    db.collection('items').insertOne({text: req.body.item}, function() {
+      res.redirect('/')
+    })
 })
 
-app.listen(3000)
+app.post('/update-item', function(req, res) {
+  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+    res.send("Success")
+  })
+})
+
+app.post('/delete-item', function(req, res) {
+  db.collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, function() {
+    res.send("Success")
+  })
+})
